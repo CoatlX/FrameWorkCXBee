a7359faa45f9684a18c16af5f2c9f35eb51d334c
CoatlX <coatlex@gmail.com>
2024-10-02 16:00:48 -0600

    Va funcionando

--- templates/includes/inc_footer_moves.php
+++ templates/includes/inc_footer_moves.php
@@ -10,6 +10,10 @@
             </div>
         </div>
     </div>
+    <script
+  src="https://code.jquery.com/jquery-3.7.1.min.js"
+  integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
+  crossorigin="anonymous"></script>
     <script src="<?php echo PLUGINS.'waitMe/waitMe.min.js'?>"></script> 
     <script src="<?php echo JS.'main.js'?>"> </script>
     
